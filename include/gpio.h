84f64ec6554c2f3e9a65b2f6a9078b7b38012fe9
Phu <phu.npt01@gmail.com>
2026-06-10 13:46:06 +0700

    fix: resolve duplicate MQTT publish and format codebase (#7)

--- include/gpio.h
+++ include/gpio.h
@@ -1,9 +1,9 @@
 #ifndef GPIO_H
 #define GPIO_H
 // --- Modbus Pins & Config ---
-#define XY485_RX          16
-#define XY485_TX          17
-#define POLL_INTERVAL_MS  2000
-#define INTER_SLAVE_MS     200
+#define XY485_RX 16
+#define XY485_TX 17
+#define POLL_INTERVAL_MS 2000
+#define INTER_SLAVE_MS 200
 
 #endif
--- include/sht40.h
+++ include/sht40.h
@@ -1,7 +1,7 @@
 #ifndef SHT40_H
 #define SHT40_H
-#define NUM_SENSORS        2
-static const uint8_t SLAVE_ADDRS[NUM_SENSORS] = { 1, 2 };
+#define NUM_SENSORS 2
+static const uint8_t SLAVE_ADDRS[NUM_SENSORS] = {1, 2};
 
 // --- MQTT Topic Definitions ---
 #define MQTT_TOPIC_TEMPLATE "sht40/sensor%d/data"
