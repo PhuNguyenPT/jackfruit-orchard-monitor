d08761ac1db1e220ea5743f3663dfc3fb5784385
phunpt <phu.npt01@gmail.com>
2026-08-12 11:39:48 +0700

    chore: untrack wifi.h, fix .gitignore

--- include/wifi.h
+++
@@ -1,9 +0,0 @@
-#ifndef WIFI_H
-#define WIFI_H
-// ==========================================
-// WIFI CONFIGURATION
-// ==========================================
-inline constexpr char WIFI_SSID[] = "anhemsoc";
-inline constexpr char WIFI_PASSWORD[] = "Meocon1976";
-
-#endif
